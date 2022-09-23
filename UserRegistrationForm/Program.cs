905a04f209191a8df6815add53444b853621207d
sayalipagar <pagarsayali565@gmail.com>
2022-09-23 17:04:04 +0530

    [sayali]Add:EnterValidEmail

--- UserRegistrationForm/Program.cs
+++ UserRegistrationForm/Program.cs
@@ -11,6 +11,9 @@
             Console.WriteLine("Enter The Last Name: ");
             string lastName = Console.ReadLine();
             form.FirstName(lastName);
+            Console.WriteLine("Enter valid email: ");
+            string emaiId = Console.ReadLine();
+            form.EmailID(emaiId);
         }
     }
 }
